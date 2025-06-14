c8ef5e31d9da53df92d48b038864092c92fe67a9
Vinod663 <vinodfernando048@gmail.com>
2025-06-14 23:51:21 +0530

    Login Check

---
+++ src/com/cms/controller/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.cms.controller;
+
+import com.cms.dao.UserDao;
+import com.cms.model.User;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@WebServlet("/login") // URL mapping for the registration servlet
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        DataSource dataSource = (DataSource) getServletContext().getAttribute("dataSource");
+        UserDao userDao = new UserDao(dataSource);
+        User login = userDao.login(username, password);
+
+        if (login != null) {
+            request.getSession().setAttribute("user", login);
+            response.setStatus(jakarta.servlet.http.HttpServletResponse.SC_OK);
+            response.getWriter().write("Login successful"+
+                    "\nWelcome " + login.getUsername() +
+                    "\nYour role is " + login.getRole());
+
+        } else {
+            response.setStatus(jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Invalid username or password");
+        }
+    }
+}
--- src/com/cms/controller/registerServlet.java
+++
@@ -1,9 +0,0 @@
-package com.cms.controller;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-
-@WebServlet("/register") // URL mapping for the registration servlet
-public class registerServlet extends HttpServlet {
-
-}
