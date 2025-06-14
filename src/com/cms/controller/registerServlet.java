8cb7b7ecc8b6bca6e75d54628e77b4b9888f5f7b
Vinod663 <vinodfernando048@gmail.com>
2025-06-14 17:17:51 +0530

    Update Source root

---
+++ src/com/cms/controller/registerServlet.java
@@ -0,0 +1,9 @@
+package com.cms.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/register") // URL mapping for the registration servlet
+public class registerServlet extends HttpServlet {
+
+}
